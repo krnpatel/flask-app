41d0e56eb9eb9bfc9f9ce0a73ea161d9392c3983
kiran patel <kiran.patel@pearson.com>
2023-11-01 17:02:41 +0530

    added sample flask app for any demo

---
+++ app.py
@@ -0,0 +1,23 @@
+#Simplest app.py code for demo
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<img src=https://cataas.com/cat>"
+
+@app.route("/hello")
+def greetings():
+    return "<html><h1> Good Morning </h1></html>"
+
+@app.route("/kiran")
+def greeHello():
+    return "<html><h1> Hello Kiran </h1></html>"
+
+@app.route("/greet/<string:name>")
+def getName(name):
+    return f"<html><h1> Hello {name}</h1></html>"
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8081)  #host="0.0.0.0",port=8081
