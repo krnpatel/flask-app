6a87512140ee5ae393a6dc35f72b0e7fa19fdc54
kiran <kiran.patel@pearson.com>
2023-11-26 14:52:47 +0530

    updated project structure

---
+++ project1/app.py
@@ -0,0 +1,15 @@
+#Simplest app.py code for demo
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<html> <h1 style='color:red;'> <b> Good Morning - Project1 - V1 </b></h1></html>"
+
+@app.route("/kiran")
+def greeHello():
+    return "<html><h1 style='color:yellow;'> Hello Kiran </h1></html>"
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8081)  #host="0.0.0.0",port=8081
--- project2/app.py
+++ project2/app.py
@@ -5,19 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<img src=https://cataas.com/cat>"
-
-@app.route("/hello")
-def greetings():
-    return "<html> <h1 style='color:red;'> <b> Good Morning - V4 </b></h1></html>"
+    return "<html> <h1 style='color:green;'> <b> Good Morning - Project2 - V1 </b></h1></html>"
 
 @app.route("/kiran")
 def greeHello():
     return "<html><h1 style='color:blue;'> Hello Kiran </h1></html>"
-
-@app.route("/greet/<string:name>")
-def getName(name):
-    return f"<html><h1> Hello {name}</h1></html>"
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8081)  #host="0.0.0.0",port=8081
