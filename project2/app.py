bcf1208ae03b04353348a7363d86e862199f0591
kiran <kiran.patel@pearson.com>
2023-11-28 14:54:30 +0530

    v4

--- project2/app.py
+++ project2/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<html> <h1 style='color:green;'> <b> Good Morning - Project2 - V2 </b></h1></html>"
+    return "<html> <h1 style='color:green;'> <b> Good Morning - Project2 - V4 </b></h1></html>"
 
 @app.route("/kiran")
 def greeHello():
